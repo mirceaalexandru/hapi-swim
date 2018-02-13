0029761128cdd3dab08e4c75b1fced879a64b5c7
Mircea Alexandru <mircea.alexandru@gmail.com>
2018-02-14 00:08:57 +0200

    added router

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+const SwimRouter = require('./lib/router')
+const HapiPino = require('hapi-pino')
 const Swim = require('./lib/swim')
 const Server = require('./lib/server')
+
+async function healthHandler(request, h) {
+  const swim = request.swim
+
+  await request.swimRouter.send({method: 'get', path: '/malex'}, request)
+  await request.swimRouter.send({method: 'get', path: '/test'}, request)
+  await request.swimRouter.send({method: 'get', path: '/non-existing'}, request)
+  return Object.assign({}, {members: swim.members()}, {me: swim.whoami()})
+};
+
+const healthEndpoint = {
+  path: '/health',
+  method: 'GET',
+  handler: healthHandler
+}
 
 process.on('unhandledRejection', error => {
   console.log('unhandledRejection', error);
   process.exit(1)
 });
 
-async function start(options, plugins, routes) {
-  const server = await Server.start(options.server, plugins, routes)
+const internals = {}
+
+async function start(options, plugins) {
+  const defaultPlugins = [
+    HapiPino
+  ]
+  const registerPlugins = plugins.concat(defaultPlugins)
+  const server = await Server.start(options.server, registerPlugins)
+  server.route(healthEndpoint)
   await Swim.start(options.swim, server)
+  server.decorate('request', 'swim', Swim.instance())
+  server.decorate('request', 'swimRouter', SwimRouter(Swim.instance()))
 }
 
 module.exports = {
-  start
+  start,
+  server: internals.server
 }
--- server.js
+++ server.js
@@ -1,29 +1,11 @@
 const server = require('./index')
-const pino = require('hapi-pino')
 const Config = require('./config/default')
-
-function routes() {
-  const healthHandler = function (request, h) {
-
-    return 'OK'
-  };
-
-  return [{
-    path: '/health',
-    method: 'GET',
-    handler: healthHandler
-  }]
-}
-
 
 async function start() {
   const config = await Config()
-  server.start(
+  await server.start(
     config,
-    [
-      pino
-    ],
-    routes()
+    []
   )
 }
 
